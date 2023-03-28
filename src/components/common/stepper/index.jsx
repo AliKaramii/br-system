1f2ca29102d9ca4b2250e8899f4477dfe1636a6f
AliKarami_h <me.karamiali@gmail.com>
2023-03-28 11:43:01 +0430

    Bugfix> 	Slylis added to project 	Stepper component rtl bug resolved
    	modified:   package-lock.json
    	modified:   package.json
    	modified:   src/components/common/stepper/index.jsx
    	modified:   src/pages/login/style.module.scss

--- src/components/common/stepper/index.jsx
+++ src/components/common/stepper/index.jsx
@@ -3,20 +3,35 @@ import Box from "@mui/material/Box";
 import Stepper from "@mui/material/Stepper";
 import Step from "@mui/material/Step";
 import StepLabel from "@mui/material/StepLabel";
+// for rtl
+import { CacheProvider } from "@emotion/react";
+import rtlPlugin from "stylis-plugin-rtl";
+import createCache from "@emotion/cache";
+import { prefixer } from "stylis";
+const cacheRtl = createCache({
+  key: "muirtl",
+  stylisPlugins: [prefixer, rtlPlugin],
+});
+// function RTL(props) {
+//   return <CacheProvider value={cacheRtl}>{props.children}</CacheProvider>;
+// }
+//
 
 const steps = ["انتخاب کافی شاپ", "ورود اطلاعات ", " بررسی و پرداخت"];
 
 function FormStepper({ activeStep }) {
   return (
-    <Box sx={{ width: "100%", direction: "ltr", paddingBottom: "6vh" }}>
-      <Stepper activeStep={activeStep} alternativeLabel>
-        {steps.map((label) => (
-          <Step key={label}>
-            <StepLabel>{label}</StepLabel>
-          </Step>
-        ))}
-      </Stepper>
-    </Box>
+    <CacheProvider value={cacheRtl}>
+      <Box sx={{ width: "100%", direction: "ltr", paddingBottom: "6vh" }}>
+        <Stepper activeStep={activeStep} alternativeLabel>
+          {steps.map((label) => (
+            <Step key={label}>
+              <StepLabel>{label}</StepLabel>
+            </Step>
+          ))}
+        </Stepper>
+      </Box>
+    </CacheProvider>
   );
 }
 
